bd7601856688c7722bb1aa6531bea53ede3e8d7e
ArturLozinski <s371452@oslomet.no>
2024-02-02 22:21:45 +0100

    function validating movie input and button to buy tickets

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,11 @@
+function buyTicket() {
+
+}
+
+function validateMovieInput() {
+    let movieInput = document.getElementById("mySelect");
+    let optionSelIndex = e.options[e.selectedIndex].value;
+    if (optionSelIndex == 0) {
+        alert("Please select a movie");
+    }
+}
