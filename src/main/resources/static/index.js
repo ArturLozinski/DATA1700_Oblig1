ebc14e28ce5f987a63c4df693badcb12ecf45a36
ArturLozinski <s371452@oslomet.no>
2024-02-09 14:27:04 +0100

    Commit of files not under version control

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,12 +1,43 @@
-function buyTicket() {
-    let numberOfTickets = document.getElementById("number");
-    if(numberOfTickets = null || 0){
-        alert("Please enter a valid number of tickets")
-    }
+const tickets = [];
 
-    let movieInput = document.getElementById("mySelect");
-    let optionSelIndex = e.options[e.selectedIndex].value;
-    if (optionSelIndex == 0) {
-        alert("Please select a movie");
+function buyTicket() {
+    const input = {
+        movie: document.getElementById("movieSelect").value,
+        tickets: document.getElementById("numberOfTickets").value,
+        first_name: document.getElementById("fname").value,
+        last_name: document.getElementById("lname").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value
     }
+    tickets.push(input);
+    displayTicketTable();
+
+    document.getElementById("movieSelect").value = "0"; // Reset select dropdown to default option
+    document.getElementById("numberOfTickets").value = ""; // Clear number of tickets input
+    document.getElementById("fname").value = ""; // Clear first name input
+    document.getElementById("lname").value = ""; // Clear last name input
+    document.getElementById("email").value = ""; // Clear email input
+    document.getElementById("phone").value = ""; // Clear phone input
+}
+
+function displayTicketTable() {
+    let out = "<table><tr>" +
+        "<th>Movie</th><th>Tickets</th><th>First name</th><th>Last name</th><th>E-Mail</th><th>Phonenumber</th>" +
+        "</tr>";
+    for (let p of tickets) {
+        out += "<tr>";
+        out += "<td>" + p.movie + "</td>" +
+            "<td>" + p.tickets + "</td>" +
+            "<td>" + p.first_name + "</td>" +
+            "<td>" + p.last_name + "</td>" +
+            "<td>" + p.email + "</td>" +
+            "<td>" + p.phone + "</td>";
+        out += "</tr>";
+    }
+    document.getElementById("ticketTable").innerHTML = out;
+}
+
+function dropTable() {
+    tickets.pop();
+    displayTicketTable()
 }
