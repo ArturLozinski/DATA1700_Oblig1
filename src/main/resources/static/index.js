9c9b86ff6ca41786d5833c42d79143dd22e1f212
ArturLozinski <s371452@oslomet.no>
2024-02-16 12:27:12 +0100

    Input validation functions and booleans, cleanup of code format, proper array function and variable definitions.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,18 +1,77 @@
-let tickets = [];
+let allTickets = [];
 
 function buyTicket() {
-    let input = {
-        movie: document.getElementById("movieSelect").value,
-        tickets: document.getElementById("number").value,
-        first_name: document.getElementById("fname").value,
-        last_name: document.getElementById("lname").value,
-        email: document.getElementById("email").value,
-        phone: document.getElementById("phone").value
+
+    let movie = document.getElementById("movieSelect").value;
+    let tickets = document.getElementById("number").value;
+    let first_name = document.getElementById("fname").value;
+    let last_name = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+
+    let errorMessage = false;
+
+    if (movie === "0") {
+        document.getElementById("invalidChoice").innerHTML = "Please select a movie";
+        errorMessage = true;
     }
 
-    tickets.push(input);
-    displayTicketTable();
+    //let numberOfTickets = document.getElementById("number").value;
+    if (tickets === null || tickets <= 0) {
+        document.getElementById("invalidNumber").innerHTML = "Please enter a valid amount of tickets";
+        errorMessage = true;
+    }
 
+    if (first_name === "") {
+        document.getElementById("invalidFirstName").innerHTML = "Please fill out your first name";
+        errorMessage = true;
+    }
+
+    if (last_name === "") {
+        document.getElementById("invalidLastName").innerHTML = "Please fill out your last name";
+        errorMessage = true;
+    }
+
+    if (!validEmail(email) || email === "") {
+        document.getElementById("invalidEmail").innerHTML = "Please enter a valid email adress";
+        errorMessage = true;
+    }
+
+    if (!validPhone(phone) || phone === "") {
+        document.getElementById("invalidPhone").innerHTML = "Please enter a valid phone number";
+        errorMessage = true;
+    }
+
+    if (!errorMessage) {
+        let ticketInput = {
+            movie: movie,
+            tickets: tickets,
+            first_name: first_name,
+            last_name: last_name,
+            email: email,
+            phone: phone
+        }
+        console.log(ticketInput);
+        allTickets.push(ticketInput);
+        displayTicketTable();
+        clearInput();
+        clearErrorMessages();
+    }
+}
+
+// Email validation function
+function validEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// Phone validation function
+function validPhone(phone) {
+    let re = /^\d{8}$/;
+    return re.test(phone);
+}
+
+function clearInput() {
     document.getElementById("movieSelect").value = "0";
     document.getElementById("number").value = "";
     document.getElementById("fname").value = "";
@@ -25,7 +84,7 @@ function displayTicketTable() {
     let out = "<table><tr>" +
         "<th>Movie</th><th>Tickets</th><th>First name</th><th>Last name</th><th>E-Mail</th><th>Phonenumber</th>" +
         "</tr>";
-    for (let p of tickets) {
+    for (let p of allTickets) {
         out += "<tr>";
         out += "<td>" + p.movie + "</td>" +
             "<td>" + p.tickets + "</td>" +
@@ -38,7 +97,17 @@ function displayTicketTable() {
     document.getElementById("ticketTable").innerHTML = out;
 }
 
+function clearErrorMessages() {
+    document.getElementById("invalidChoice").innerHTML = "";
+    document.getElementById("invalidNumber").innerHTML = "";
+    document.getElementById("invalidFirstName").innerHTML = "";
+    document.getElementById("invalidLastName").innerHTML = "";
+    document.getElementById("invalidEmail").innerHTML = "";
+    document.getElementById("invalidPhone").innerHTML = "";
+
+}
+
 function dropTable() {
-    tickets = [];
+    allTickets = [];
     displayTicketTable()
 }
